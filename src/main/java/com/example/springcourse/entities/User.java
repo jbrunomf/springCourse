c3589765f01964c53fd28492d39629bb62b76bec
João Bruno Moura Fernandes <jbrunomf@outlook.com>
2022-06-23 21:27:57 -0400

    adicionando dependencias SPRING JPA E O H2. / Definindo configuração no .properties

--- src/main/java/com/example/springcourse/entities/User.java
+++ src/main/java/com/example/springcourse/entities/User.java
@@ -1,10 +1,15 @@
 package com.example.springcourse.entities;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
+@Table(name = "Users")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
